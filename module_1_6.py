4a8be2c264fd4373d1c9e6a4f39fab44cafce392
Paul Tupchiy <56828670+Rayanhart@users.noreply.github.com>
2024-12-02 19:39:51 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,5 @@
+lessons_lernt = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_per_lesson = hours_spent / lessons_lernt
+print('Курс: ', course_name, ', всего задач:', lessons_lernt, ', затрачено часов: ', hours_spent, ', среднее время выполнения ', time_per_lesson, ' часа.')
